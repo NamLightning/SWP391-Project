0e9f35a49a78399d3a6a555d2db8d1fead085942
NamLightning <91459140+NamLightning@users.noreply.github.com>
2024-11-13 11:22:23 +0700

    Update Constants.java

--- src/java/google_context/Constants.java
+++ src/java/google_context/Constants.java
@@ -2,13 +2,4 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package google_context;
-
-public class Constants {
-  public static String GOOGLE_CLIENT_ID = "697015326499-mehleirgoloeb1m97o29jueu5rduj277.apps.googleusercontent.com";
-  public static String GOOGLE_CLIENT_SECRET = "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";
-  public static String GOOGLE_REDIRECT_URI = "http://localhost:8084/PRJ301-FinalProject/LoginControl";
-  public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
-  public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
-  public static String GOOGLE_GRANT_TYPE = "authorization_code";
-}
+n
