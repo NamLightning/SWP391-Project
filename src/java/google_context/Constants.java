afaa985419d64eca446a58551ceacce5355ae8a0
NamLightning <91459140+NamLightning@users.noreply.github.com>
2024-11-13 11:19:14 +0700

    Update Constants.java

--- src/java/google_context/Constants.java
+++ src/java/google_context/Constants.java
@@ -5,8 +5,8 @@
 package google_context;
 
 public class Constants {
-  public static String GOOGLE_CLIENT_ID = "697015326499-mehleirgoloeb1m97o29jueu5rduj277.apps.googleusercontent.com";
-  public static String GOOGLE_CLIENT_SECRET = "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";
+  public static String GOOGLE_CLIENT_ID = "";
+  public static String GOOGLE_CLIENT_SECRET = "";
   public static String GOOGLE_REDIRECT_URI = "http://localhost:8084/PRJ301-FinalProject/LoginControl";
   public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
   public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
